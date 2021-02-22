270a5d6b5bbc57e8cd8477151680fcf7d44620e8
mzubkova <59561550+mzubkova@users.noreply.github.com>
2021-02-22 19:42:10 +0200

    added mobile adaptive/ burger menu

--- js/index.js
+++ js/index.js
@@ -35,3 +35,14 @@ $(function () {
 $(function () {
   $("#tabs").tabs();
 });
+
+$("#navToggle").click(function () {
+  $(this).toggleClass("active");
+  $(".overlay").toggleClass("open");
+  // this line ▼ prevents content scroll-behind
+  $("body").toggleClass("locked");
+});
+$(".overlay").click(function () {
+  $(this).removeClass("open");
+  $(".navBurger").removeClass("active");
+});
